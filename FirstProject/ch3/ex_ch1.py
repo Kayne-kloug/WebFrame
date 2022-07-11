210aefea0a9319f59f6a203f64807f9ae7b3d160
K <rlarudfo91@gmail.com>
2022-07-11 12:49:28 +0900

    first commit

---
+++ FirstProject/ch3/ex_ch1.py
@@ -0,0 +1,7 @@
+#import urllib.request
+#print(urllib.request.urlopen("http://www.example.com").read().decode('utf-8'))
+
+GET /book/shakespeare HTTP/1.1
+Host: www.example.com:8080
+
+#http://www.example.com:8080/book/shakespeare HTTP/1.1
